fd159310e4a40b01a402705ab427c641b4c35b1a
rahula1008 <e1136941@u.nus.edu>
2025-12-20 20:59:49 +0800

    chore: refactor code for readability

--- backend/controllers/topicControllers.go
+++ backend/controllers/topicControllers.go
@@ -12,14 +12,6 @@ import (
 	"github.com/rahula1008/Web_Forum/models"
 )
 
-type Response struct {
-	Success bool        `json:"success"`
-	Data    interface{} `json:"data,omitempty"`
-	Message string      `json:"message,omitempty"`
-	Error   string      `json:"error,omitempty"`
-	Code    int         `json:"code,omitempty"`
-}
-
 const (
 	getTopicsFailedMessage             = "Failed to get all the topics"
 	getTopicByIDFailedMessage          = "Failed to get this topic ID"
--- backend/controllers/utils.go
+++ backend/controllers/utils.go
@@ -6,6 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Response struct {
+	Success bool        `json:"success"`
+	Data    interface{} `json:"data,omitempty"`
+	Message string      `json:"message,omitempty"`
+	Error   string      `json:"error,omitempty"`
+	Code    int         `json:"code,omitempty"`
+}
+
 func SendStatusOKResponse(c *gin.Context, message string) {
 	c.JSON(http.StatusOK, Response{
 		Success: true,
