a50674e29c85753ef74bcc54f749a10cb7fa6bdd
rahula1008 <e1136941@u.nus.edu>
2026-01-02 15:35:05 +0800

    feat: add basic home page

---
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+    return (
+        <>
+            <div>
+                <title>Home Page</title>
+            </div>
+            <div>
+                <h1>NUS FORUMS</h1>
+                <p>HI THERE</p>
+            </div>
+        </>
+    )
+}
