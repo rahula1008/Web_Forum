82cbfc61ffdfb76d5caf94595a44241042105765
rahula1008 <e1136941@u.nus.edu>
2026-01-02 21:34:59 +0800

    configure cors

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/rahula1008/Web_Forum/initializers"
 	"github.com/rahula1008/Web_Forum/routes"
@@ -18,6 +19,14 @@ func main() {
 			"message": "pong",
 		})
 	})
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"http://localhost:5173"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+		// AllowCredentials: true,
+		// MaxAge:           12 * time.Hour,
+	}))
 	routes.SetupTopicRoutes(router)
 	routes.SetupUserRoutes(router)
 	routes.SetupPostRoutes(router)
