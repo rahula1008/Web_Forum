f5831ffdade699859baa78ca8b378dfc42254d77
rahula1008 <e1136941@u.nus.edu>
2026-01-07 13:47:04 +0800

    feat: enable credentials on cors settings

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/rahula1008/Web_Forum/initializers"
@@ -20,12 +22,12 @@ func main() {
 		})
 	})
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:  []string{"http://localhost:5173"},
-		AllowMethods:  []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:  []string{"Origin", "Content-Type", "Accept", "Authorization"},
-		ExposeHeaders: []string{"Content-Length"},
-		// AllowCredentials: true,
-		// MaxAge:           12 * time.Hour,
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
 	}))
 	routes.SetupTopicRoutes(router)
 	routes.SetupUserRoutes(router)
