39fb60d00e575c7992e3ed0e15ccf949f2ff8494
rahula1008 <e1136941@u.nus.edu>
2026-01-21 16:33:10 +0800

    add frontend url to cors

--- backend/main.go
+++ backend/main.go
@@ -22,7 +22,7 @@ func main() {
 		})
 	})
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://brilliant-kelpie-36e459.netlify.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
