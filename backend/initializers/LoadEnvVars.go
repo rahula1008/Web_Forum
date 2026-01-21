4a5e2950dae59108cb0a96e8a71a3b729d6d925e
rahula1008 <e1136941@u.nus.edu>
2026-01-21 14:30:53 +0800

    chore: only load env variables in local

--- backend/initializers/LoadEnvVars.go
+++ backend/initializers/LoadEnvVars.go
@@ -2,13 +2,17 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file: ", err)
+	if os.Getenv("ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file: ", err)
+		}
 	}
+
 }
